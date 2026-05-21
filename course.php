7b64517252bd05ba83d35637415337e0618b80a0
albert123-arch <iskhakovalbert1@gmail.com>
2026-05-21 22:17:17 +0500

    Stop tracking database SQL files

--- course.php
+++ course.php
@@ -17,18 +17,14 @@ include __DIR__ . '/includes/layout/header.php';
 </div>
 
 <ul class="nav nav-tabs mb-3" role="tablist">
-    <li class="nav-item"><button class="nav-link active" data-bs-toggle="tab" data-bs-target="#overview" type="button"><?= e(t('overview')) ?></button></li>
-    <li class="nav-item"><button class="nav-link" data-bs-toggle="tab" data-bs-target="#chapters" type="button"><?= e(t('chapters')) ?></button></li>
+    <li class="nav-item"><button class="nav-link active" data-bs-toggle="tab" data-bs-target="#chapters" type="button"><?= e(t('chapters')) ?></button></li>
     <li class="nav-item"><a class="nav-link" href="<?= e(practice_url($course['slug'])) ?>"><?= e(t('practice')) ?></a></li>
     <li class="nav-item"><button class="nav-link" data-bs-toggle="tab" data-bs-target="#worksheets" type="button"><?= e(t('worksheets')) ?></button></li>
     <?php if (is_teacher()): ?><li class="nav-item"><button class="nav-link" data-bs-toggle="tab" data-bs-target="#teacher" type="button"><?= e(t('teacher_notes')) ?></button></li><?php endif; ?>
 </ul>
 
 <div class="tab-content">
-    <section class="tab-pane fade show active" id="overview">
-        <div class="admin-panel p-3 math-content"><?= $course['description_html'] ?: e(t('coming_soon')) ?></div>
-    </section>
-    <section class="tab-pane fade" id="chapters">
+    <section class="tab-pane fade show active" id="chapters">
         <div class="vstack gap-3">
             <?php foreach ($chapters as $chapter): include __DIR__ . '/includes/components/chapter-card.php'; endforeach; ?>
             <?php if (!$chapters): ?><div class="alert alert-info"><?= e(t('no_items')) ?></div><?php endif; ?>
@@ -40,4 +36,3 @@ include __DIR__ . '/includes/layout/header.php';
     <?php if (is_teacher()): ?><section class="tab-pane fade" id="teacher"><div class="admin-panel p-3"><?= e(t('teacher_only')) ?></div></section><?php endif; ?>
 </div>
 <?php include __DIR__ . '/includes/layout/footer.php'; ?>
-
--- database/drop_legacy_summary_columns.sql
+++
@@ -1,8 +0,0 @@
-SET NAMES utf8mb4;
-
-ALTER TABLE course_texts
-    DROP COLUMN IF EXISTS summary_html,
-    DROP COLUMN IF EXISTS overview_html;
-
-ALTER TABLE chapter_texts
-    DROP COLUMN IF EXISTS summary_html;
